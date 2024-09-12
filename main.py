779b0cca9d53429cf46f4245adda779533a33cef
JuanCarlosGA <jucar5454@gmail.com>
2024-09-12 10:35:09 -0500

    Implementacion nueva funcionalidad Resta

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def suma(x, y):
     return x + y
 
+def resta(x, y):
+    return x - y
+
+# Realizar la suma
 resultado = suma(2, 3)
+print(resultado)
+
+# Realizar la resta
+resultado = resta(2, 3)
 print(resultado)
