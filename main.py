9f339eaa7c9188783b7ab7cab550dd14b1fc5cce
JuanCarlosGA <jucar5454@gmail.com>
2024-09-12 10:39:49 -0500

    Se documento la suma

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+"""
+# Author: Juan Carlos Giraldo
+# Date: 2021/07/10
+# Description: This is a simple function program that adds two numbers.
+# Params: x, y (int)
+# Return: x + y (int)
+"""
 def suma(x, y):
     return x + y
 
+"""
+# Test
+# Expected: 5
+"""
 resultado = suma(2, 3)
 print(resultado)
