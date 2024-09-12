ad8fa1f93ef2c00d5caceaedfcbeb734bc53d161
JuanCarlosGA <jucar5454@gmail.com>
2024-09-12 10:44:12 -0500

    Se documento la Reta

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 def suma(x, y):
     return x + y
-
+"""
+# A simple function that subtracts two numbers
+# Parameters: x, y
+# Returns: x - y
+"""
 def resta(x, y):
     return x - y
 
-# Realizar la suma
 resultado = suma(2, 3)
 print(resultado)
 
-# Realizar la resta
+"""
+# Test
+# Espected output: -1
+"""
 resultado = resta(2, 3)
 print(resultado)
